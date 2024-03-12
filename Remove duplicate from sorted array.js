f78b647c93025abf8be4bc090be793c7d0a27843
Mohammed Thasleem MK <mohammedthasleemmk6604@gmail.com>
2024-03-12 09:38:11 +0530

    Remove duplicate from sorted array

--- Remove duplicate from sorted array.js
+++ Remove duplicate from sorted array.js
@@ -1,8 +1,13 @@
-const arr = [1, 2, 1, 3, 4, 3, 2];
-var removeDuplicate = (arr) => {
-	var arr = arr;
-	
-	return arr;
-}
-var Sample = removeDuplicate(arr);
-console.log(Sample);
+var Word = prompt("Enter the sentence");
+var capitalizeTitle = function (title) {
+  var arr = title.toLowerCase().split(" ");
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].length > 2) {
+      var Cap = arr[i].split("");
+      Cap[0] = Cap[0].toUpperCase();
+      arr[i] = Cap.join("");
+    }
+  }
+  return arr.join(" ");
+};
+console.log(capitalizeTitle(Word));
